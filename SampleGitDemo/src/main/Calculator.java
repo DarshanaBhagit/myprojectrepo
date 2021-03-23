141802494cf1be03b84e09ea953b22c0a18db363
Mona <Mona@LAPTOP-6EVT6MO3>
2021-03-23 10:18:25 +0530

    Added cube function

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -32,5 +32,9 @@ public class Calculator {
 	public static void mod(int num1,int num2){
 	      System.out.println(num1%num2);
 	  }
+	
+	public static void cube(int num1,int num2){
+	      System.out.println(num1*num2*num1);
+	  }
 }
 
