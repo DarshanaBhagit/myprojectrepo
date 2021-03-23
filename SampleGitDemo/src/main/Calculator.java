5acd6983704e964bdeaa549b47932be790812347
Mona <Mona@LAPTOP-6EVT6MO3>
2021-03-23 10:19:54 +0530

    added quadra function

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -24,5 +24,10 @@ public class Calculator {
 	public static void square(int num1,int num2){
 	      System.out.println(num1*num2);
 	  }
+	public static void quadra(int num1,int num2){
+	      System.out.println(num1*num1*num2);
+	  }
+	
+	
 
 }
